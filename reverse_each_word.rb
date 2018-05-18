4f1542ab0881340549999144a4ccec229437b219
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-18 11:25:40 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,21 @@
 def reverse_each_word(string)
   new_string = []
+  new_array = []
   array = string.split(" ")
   array.each do |word|
     new_array.push(word.reverse)
   end
-  new_string.join(" ")
+  new_string = new_array.join(" ")
 end
+
+def reverse_each_word(string)
+  new_string = []
+  new_array = []
+  array = string.split(" ")
+  array.collect do |word|
+    new_array.push(word.reverse)
+  end
+  new_string = new_array.join(" ")
+end
+
+#reverse_each_word("Hello my name is daniel!")
