32b87f201af601b69984cb088f9c9f2fcea9777c
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-18 11:23:20 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,22 @@
+require 'pry'
 def reverse_each_word(string)
   new_string = []
+  new_array = []
   array = string.split(" ")
   array.each do |word|
     new_array.push(word.reverse)
   end
-  new_string.join(" ")
+  new_string = new_array.join(" ")
 end
+
+def reverse_each_word(string)
+  new_string = []
+  array = string.split(" ")
+  array.collect do |word|
+    "#{word.reverse}"
+  end
+  new_string = array.join(" ")
+  binding.pry
+end
+
+reverse_each_word("Hello my name is daniel!")
